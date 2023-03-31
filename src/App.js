11b8bb5b66c9a679eefb92f0a7ba738e7cadb75d
Matthew Leslie <matthewleslie155@gmail.com>
2023-03-31 09:38:39 -0400

    added elements of nav

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import ScoresPage from './pages/Scores/ScoresPage';
+import TopNav from './components/topnav/Topnav';
+import React, { useState } from 'react';
 
 function App() {
+  // const HomeMenu = () {
+  //   "title": "NFLF",
+  //   "TOP",
+  //   "NFL",
+  //   "NBA",
+  //   "NCAAF",
+  //   "MLB",
+  //   "NHL"
+  // }
+  const [defaultMenu, setDefaultMenu] = useState("NFLF");
+  const [scoreMenu, setScoreMenu] = useState("Score Menu");
+
+
   return (
-
-
-    <BrowserRouter>
+    <BrowserRouter> 
+        <TopNav />
         <Routes>
             <Route path={"/"} element={<HomePage />}   />
             <Route path={"/scores"} element={<ScoresPage />}  />
-       
+
             
         </Routes>
     </BrowserRouter>
