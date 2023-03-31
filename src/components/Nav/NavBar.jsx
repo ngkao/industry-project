e5a12cd7edc45dc87d9611d1c10786f3aee3b508
ngkao <ng.kao.ca@gmail.com>
2023-03-31 12:27:01 -0400

    add icons

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -7,6 +7,9 @@ import Bet from "../../assets/icon/bet.png"
 import BetBlue from "../../assets/icon/bet-bl.png"
 import { Link, useNavigate } from 'react-router-dom';
 // import BetPage from '../../pages/Bet/BetPage';
+import Star from "../../assets/icon/star.png"
+import Frame from "../../assets/icon/frame.png"
+
 
 
 const NavBar = ({isScoreSelected}) => {
@@ -33,7 +36,7 @@ const NavBar = ({isScoreSelected}) => {
                     </div>
                 )}
                 <div className="nav__item">
-                    <img className="nav__icon" src="/"/>
+                    <img className="nav__icon" src={Star}/>
                     <p className="nav__heading">Favorites</p>
                 </div>
                 {isScoreSelected ? (
@@ -49,7 +52,7 @@ const NavBar = ({isScoreSelected}) => {
                     </div>
                 )}
                 <div className="nav__item">
-                    <img className="nav__icon" src="/"/>
+                    <img className="nav__icon" src={Frame}/>
                     <p className="nav__heading">Leagues</p>
                 </div>
             </div>
