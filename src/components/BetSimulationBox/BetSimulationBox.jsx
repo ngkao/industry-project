37d155fbc648550563075cf202768e0303ca6967
ngkao <ng.kao.ca@gmail.com>
2023-03-31 12:11:54 -0400

    finish the simulation box

--- src/components/BetSimulationBox/BetSimulationBox.jsx
+++ src/components/BetSimulationBox/BetSimulationBox.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import "./BetSimulationBox.scss"
+import { useState } from 'react';
+import ArrowDown from "../../assets/icon/arrow-left.png"
 
 const BetSimulationBox = () => {
+
+    const [selected, setSelected] = useState(false);
+
+    const handleClick = () => {
+        console.log("BUTTON")
+        setSelected(true);
+    }
+
     return (
         <div className="betsimulationbox">
             <div className="betsimulationbox__title-ctr">
-                <p className="betsimulationbox__title">STRAIGHT</p>
-                <p className="betsimulationbox__title">PARALAY</p>
+                <p className="betsimulationbox__title betsimulationbox__title--selected">STRAIGHT</p>
+                <p className="betsimulationbox__title betsimulationbox__title--unselected">PARALAY</p>
             </div>
             <div className="betsimulationbox__subtitle-ctr">
                 <p className="betsimulationbox__title-rate">Cardinals</p>
                 <p className="betsimulationbox__rate">-1.5</p>
+                <img src={ArrowDown}/>
             </div>
             <div>
                 <div className="betsimulationbox__row1">
-                    <p className="betsimulationbox__row1-box">+$20</p>
+                    <p 
+                        onClick={handleClick} 
+                        className={`betsimulationbox__row1-box ${selected ? "betsimulationbox__row1-box--selected" : ""}`}
+                    >+$20</p>
                     <p className="betsimulationbox__row1-box">$50</p>
                     <p className="betsimulationbox__row1-box">$100</p>
                     <p className="betsimulationbox__row1-box">$200</p>
@@ -22,16 +36,16 @@ const BetSimulationBox = () => {
                 <div className="betsimulationbox__row2">
                     <div className="betsimulationbox__row2-box">
                         <p className="betsimulationbox__row2-title">Bet</p>
-                        <p className="betsimulationbox__row2-num">$0.00</p>
+                        <p className={`betsimulationbox__row2-num ${selected ? "betsimulationbox__row2-num--selected" : ""}`}>{selected ? "$20.00" : "$0.00"}</p>
                     </div>
                     <div className="betsimulationbox__row2-box">
                         <p className="betsimulationbox__row2-title">Bet</p>
-                        <p className="betsimulationbox__row2-num">$0.00</p>
+                        <p className={`betsimulationbox__row2-num ${selected ? "betsimulationbox__row2-num--selected" : ""}`}>{selected ? "$30.00" : "$0.00"}</p>
                     </div>   
                 </div>
                 <div className="betsimulationbox__payout-ctr">
                     <p className="betsimulationbox__payout">PAYOUT</p>
-                    <p className="betsimulationbox__payout">$50.00</p>
+                    <p className="betsimulationbox__payout">{selected ? "$50.00" : "$0.00"}</p>
                 </div>
             </div>
         </div>
